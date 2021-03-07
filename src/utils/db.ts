1b5e86f5a175fae3b9b7de0d22fe877362ca3cbd
wangdong <wangdong7871@100.me>
2021-03-08 01:16:34 +0800

    feat: 删除sequelize框架

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,11 +1,3 @@
-import { Sequelize, DataTypes, Model } from "sequelize";
 
-// 手动创建一个库叫wd_shop，然后执行该语句
-const sequelize = new Sequelize('wd_shop', 'root', 'root', {
-  host: 'localhost',
-  dialect: 'mysql',
-});
+// 'wd_shop', 'root', 'root'
 
-export default sequelize;
-
-//  sequelize.authenticate();
