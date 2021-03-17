c9c0a5ce37a2e4bd09570a412fbcbe524d7cb0d2
wangdong <wangdong7871@100.me>
2021-03-17 23:53:20 +0800

    feat: 优化参数

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -5,9 +5,9 @@ function createToken(payload: object) {
   return jwt.sign(payload, secretToken, { expiresIn: 60 });
 }
 
-async function decodeToken(token: string) {
-  var decoded = jwt.verify(token, secretToken);
-  console.log(decoded) // b
+async function decodeToken(token: string): Promise<string | object> {
+  const decoded = jwt.verify(token, secretToken);
+  return decoded;
 }
 
 export {
